1e1f5c2fe7efebd81224fad9521dffa640d72195
offor <offorchukwuebuka3@gmail.com>
2024-12-06 17:39:39 +0100

    added more customization to ant bar

--- src/sections/login/login-view.jsx
+++ src/sections/login/login-view.jsx
@@ -6,8 +6,6 @@ import Box from '@mui/material/Box';
 import Link from '@mui/material/Link';
 import Card from '@mui/material/Card';
 import Stack from '@mui/material/Stack';
-import Button from '@mui/material/Button';
-import Divider from '@mui/material/Divider';
 import TextField from '@mui/material/TextField';
 import Typography from '@mui/material/Typography';
 import IconButton from '@mui/material/IconButton';
@@ -59,7 +57,7 @@ export default function LoginView() {
         router.push('/');
       } else {
         setError('Login failed. Please check your credentials.');
-        
+
       }
       // eslint-disable-next-line no-shadow
     } catch (error) {
@@ -138,9 +136,16 @@ export default function LoginView() {
             </Stack>
 
             <Stack direction="row" alignItems="center" justifyContent="flex-end" sx={{ my: 3 }}>
-              <Link variant="subtitle2" underline="hover">
+              <Link
+                href="https://otpninja.com/forgot"
+                variant="subtitle2"
+                underline="hover"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
                 Forgot password?
               </Link>
+
             </Stack>
 
             <LoadingButton
@@ -157,43 +162,7 @@ export default function LoginView() {
             {error && <Typography color="error">{error}</Typography>}
           </form>
 
-          <Divider sx={{ my: 3 }}>
-            <Typography variant="body2" sx={{ color: 'text.secondary' }}>
-              OR
-            </Typography>
-          </Divider>
 
-          <Stack direction="row" spacing={2}>
-            <Button
-              fullWidth
-              size="large"
-              color="inherit"
-              variant="outlined"
-              sx={{ borderColor: alpha(theme.palette.grey[500], 0.16) }}
-            >
-              <Iconify icon="eva:google-fill" color="#DF3E30" />
-            </Button>
-
-            <Button
-              fullWidth
-              size="large"
-              color="inherit"
-              variant="outlined"
-              sx={{ borderColor: alpha(theme.palette.grey[500], 0.16) }}
-            >
-              <Iconify icon="eva:facebook-fill" color="#1877F2" />
-            </Button>
-
-            <Button
-              fullWidth
-              size="large"
-              color="inherit"
-              variant="outlined"
-              sx={{ borderColor: alpha(theme.palette.grey[500], 0.16) }}
-            >
-              <Iconify icon="eva:twitter-fill" color="#1C9CEA" />
-            </Button>
-          </Stack>
         </Card>
       </Stack>
     </Box>
